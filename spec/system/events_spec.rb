b2517aa8d211489da61d35f645aba581a088465a
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-09-16 09:58:07 +0900

    Test: 予定の表示と作成のシステムスペックを追加

---
+++ spec/system/events_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Events', type: :system do
+  describe 'show events' do
+    let(:alice) { create :user }
+    let!(:event) { create :event, start_time: DateTime.now, user: alice }
+
+    it 'shows events' do
+      sign_in alice
+      visit events_path
+      expect(page).to have_content event.name
+      expect(page).to have_content I18n.l(event.start_time)
+    end
+  end
+
+  describe 'create event' do
+    let(:alice) { create :user }
+    let(:event) { build_stubbed :event, start_time: DateTime.now }
+
+    it 'creates event', js: true do
+      sign_in alice
+      visit events_path
+      click_button '作成'
+      fill_in 'event[name]', with: event.name
+      fill_in 'event[start_time]', with: event.start_time
+      expect{
+        click_button '保存'
+      }.to change { Event.count }.by(1)
+      .and change { alice.events.count }.by(1)
+      expect(page).to have_content '予定を保存しました。'
+      expect(page).to have_content event.name
+      expect(page).to have_content I18n.l(event.start_time)
+    end
+    end
+end
