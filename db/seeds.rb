aebc9e13672ee6e075a6cf7a54dba80609ce95de
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-09-17 21:16:48 +0900

    Chore: production環境用のデモデータの作成方法を変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@ lorem_description = Faker::Lorem.paragraphs(number: 5).join(' ')
 start_day = DateTime.new(2021, 9, 1)
 last_day = start_day + rand(30).days
 
-5.times do
+20.times do
   randomized_time = Random.rand(start_day..last_day) + rand(24).hour + rand(60).minutes
   guest_user.events.create!(
     name: Faker::Hobby.activity,
     description: lorem_description,
     start_time: randomized_time,
-    end_time: randomized_time + rand(24).hours,
+    end_time: randomized_time + 1.hours,
   )
 end
 
@@ -48,5 +48,18 @@ guest_user.events.create!(
   name: 'a'*255,
   description: 'a'*2000,
   start_time: randomized_time,
-  end_time: randomized_time + rand(24).hours,
+  end_time: randomized_time + 1.hour,
 )
+
+start_day = DateTime.new(2021, 1, 1)
+last_day = DateTime.new(2021, 12, 31)
+
+180.times do
+  randomized_time = Random.rand(start_day..last_day) + rand(24).hour + rand(60).minutes
+  guest_user.events.create!(
+    name: Faker::Hobby.activity,
+    description: lorem_description,
+    start_time: randomized_time,
+    end_time: randomized_time + 1.hour,
+  )
+end
