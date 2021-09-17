116192684e254150e20381aec81f3486b6480369
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-09-17 16:56:38 +0900

    Feat: eventsテーブルにdescriptionとend_timeカラムを追加、バリデーションとそのテストを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,16 @@ class Event < ApplicationRecord
 
   validates :name, presence: true
   validates :start_time, presence: true
+  validates :end_time, presence: true
+  validate :time_validation
+
+  def time_validation
+    if start_time.blank?
+      errors.add(:start_time, 'を入力してください')
+    elsif end_time.blank?
+      errors.add(:end_time, 'を入力してください')
+    elsif start_time > end_time
+      errors.add(:end_time, 'は開始時刻以降の時間を入力してください')
+    end
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,5 +4,47 @@ RSpec.describe Event, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:start_time) }
+    it { is_expected.to validate_presence_of(:end_time) }
+
+    describe 'time_validation' do
+      context 'start_time > end_time' do
+        it 'is invalid' do
+          set_start_time = DateTime.new(2021, 1, 1, 12)
+          set_end_time = set_start_time - 1.hour
+
+          event = build_stubbed(:event, start_time: set_start_time, end_time: set_end_time)
+          expect(event.valid?).to eq false
+        end
+
+        it 'has error message' do
+          set_start_time = DateTime.new(2021, 1, 1, 12)
+          set_end_time = set_start_time - 1.hour
+
+          event = build_stubbed(:event, start_time: set_start_time, end_time: set_end_time)
+          event.valid?
+          expect(event.errors[:end_time]).to include 'は開始時刻以降の時間を入力してください'
+        end
+      end
+
+      context 'start_time == end_time' do
+        it 'is valid' do
+          set_start_time = DateTime.new(2021, 1, 1, 12)
+          set_end_time = set_start_time
+
+          event = build_stubbed(:event, start_time: set_start_time, end_time: set_end_time)
+          expect(event.valid?).to eq true
+        end
+      end
+
+      context 'start_time < end_time' do
+        it 'is valid' do
+          set_start_time = DateTime.new(2021, 1, 1, 12)
+          set_end_time = set_start_time + 1.hour
+
+          event = build_stubbed(:event, start_time: set_start_time, end_time: set_end_time)
+          expect(event.valid?).to eq true
+        end
+      end
+    end
   end
 end
